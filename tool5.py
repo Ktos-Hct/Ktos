adf459f873dff9e6740aa09441a6e2bf8b4d197e
Ktos-Hct <45519618+Ktos-Hct@users.noreply.github.com>
2019-11-12 23:01:36 +0700

    Add files via upload

---
+++ tool5.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+blind_ip = "0.0.0.0"
+blind_port = 9999
+server = socket,docket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+server.listen(5)
+print "[*] Listening on %s:%d" %(blind_ip,blind_port)
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print "[*] Recived: %s" % request
+    client_socket.send("ACK!")
+    client_socket.close()
+while True:
+    client,addr = server.accept()
+    print"[*] Accepted connection from : %s:%d" %(addr[0],addr[1])
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
